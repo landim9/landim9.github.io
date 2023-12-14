6991e77599b3f7182f8f804f9a2cdccff5dab41f
LcsCamacho <lucas.camachofilhoo@gmail.com>
2023-12-14 08:59:49 -0300

    portfolio

---
+++ assets/pages/Password Generator/assets/script/main.js
@@ -0,0 +1,24 @@
+var passbar = document.querySelector('.passbar')
+
+function generatePassword() {
+  const length = document.getElementById('length').value;
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$';
+  let password = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  document.getElementById('password').textContent = password;
+
+  passbar.style.display = 'block'
+}
+
+function copyText() {
+  var copyText = document.getElementById("password");
+  var textArea = document.createElement("textarea");
+  textArea.value = copyText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("Copy");
+  textArea.remove();
+  alert("Texto copiado para area de transferencia");
+}
