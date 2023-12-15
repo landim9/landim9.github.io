0a16715ed847cd6f336ce05e10f329315fc78e1b
LcsCamacho <lucas.camachofilhoo@gmail.com>
2023-12-15 11:17:48 -0300

    portfolio

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,3 +15,29 @@ const body = document.querySelector("body");
     window.onscroll = ()=>{
       this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
     }
+
+    let boxes = document.querySelectorAll(".box");
+
+    window.onload = function(){
+        setTimeout(() => {
+            load_bars();
+        }, 1000);
+    }
+
+    function load_bars(){
+        boxes.forEach(box => {
+            let line = box.querySelector(".line");
+            let increasing_percentage = box.querySelector(".increasing_percentage");
+            let total_percentage = box.querySelector(".total_percentage");
+            
+            let p = 0;
+            let my_interval = setInterval(() => {
+                p++;
+                line.style.width = p + "%";
+                increasing_percentage.innerHTML = p + "%";
+                if(increasing_percentage.innerHTML == total_percentage.innerHTML){
+                    clearInterval(my_interval);
+                }
+            }, 25);
+        });
+    }
