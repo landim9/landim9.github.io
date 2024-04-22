7287d52f7b3e35e4ede6ff971caac7b4d694d0e4
landim9 <landimr9des@gmail.com>
2024-04-21 21:07:59 -0300

    senai

--- assets/script/script.js
+++ assets/script/script.js
@@ -41,3 +41,30 @@ const body = document.querySelector("body");
             }, 25);
         });
     }
+
+    
+    const header = document.querySelector(".header-bg");
+    const sections = document.querySelectorAll("section");
+    
+    window.onscroll = () => {
+    
+        var fixed = 0;
+        if (window.scrollY > fixed) {
+            header.classList.add("top-header");
+        } else {
+            header.classList.remove("top-header");
+        }
+    
+        sections.forEach((section) => {
+            const top = window.scrollY;
+            const offset = section.offsetTop - 200;
+            const height = section.offsetHeight;
+            const id = section.getAttribute("id");
+    
+            if (top >= offset && top < offset + height) {
+                document.querySelector(".header-bg ul li a[href*=" + id + "]").classList.add("active"); 
+            } else {
+                document.querySelector(".header-bg ul li a[href*=" + id + "]").classList.remove("active");
+            }
+        }
+    )}
