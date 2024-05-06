1f257ca09dcf0b65224d851562abf6eccb3e668f
landim9 <landimr9des@gmail.com>
2024-05-06 09:10:31 -0300

    portfolio

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,70 +1,22 @@
-const body = document.querySelector("body");
-    const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      body.classList.add("disabled");
-    }
-    cancelBtn.onclick = ()=>{
-      body.classList.remove("disabled");
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
-    window.onscroll = ()=>{
-      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-    }
-
-    let boxes = document.querySelectorAll(".box");
-
-    window.onload = function(){
-        setTimeout(() => {
-            load_bars();
-        }, 1000);
-    }
-
-    function load_bars(){
-        boxes.forEach(box => {
-            let line = box.querySelector(".line");
-            let increasing_percentage = box.querySelector(".increasing_percentage");
-            let total_percentage = box.querySelector(".total_percentage");
-            
-            let p = 0;
-            let my_interval = setInterval(() => {
-                p++;
-                line.style.width = p + "%";
-                increasing_percentage.innerHTML = p + "%";
-                if(increasing_percentage.innerHTML == total_percentage.innerHTML){
-                    clearInterval(my_interval);
-                }
-            }, 25);
-        });
-    }
-
+        const header = document.querySelector(".header-bg");
+    const links = header.querySelectorAll("ul li a");
     
-    const header = document.querySelector(".header-bg");
-    const sections = document.querySelectorAll("section");
-    
-    window.onscroll = () => {
-    
-        var fixed = 0;
-        if (window.scrollY > fixed) {
-            header.classList.add("top-header");
-        } else {
-            header.classList.remove("top-header");
-        }
+    window.addEventListener("scroll", () => {
+        const scrollPosition = window.scrollY;
     
         sections.forEach((section) => {
-            const top = window.scrollY;
+            const id = section.getAttribute("id");
             const offset = section.offsetTop - 200;
             const height = section.offsetHeight;
-            const id = section.getAttribute("id");
     
-            if (top >= offset && top < offset + height) {
-                document.querySelector(".header-bg ul li a[href*=" + id + "]").classList.add("active"); 
-            } else {
-                document.querySelector(".header-bg ul li a[href*=" + id + "]").classList.remove("active");
+            if (scrollPosition >= offset && scrollPosition < offset + height) {
+                links.forEach((link) => {
+                    if (link.getAttribute("href").includes(id)) {
+                        link.classList.add("active");
+                    } else {
+                        link.classList.remove("active");
+                    }
+                });
             }
-        }
-    )}
+        });
+    });
